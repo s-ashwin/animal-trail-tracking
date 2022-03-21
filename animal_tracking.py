f6d0166e5382243564b60a9d5088ff5f4a75535a
s-ashwin <ashwin.sivanagarajan@gmail.com>
2022-03-21 21:22:49 +0530

    first commit

---
+++ animal_tracking.py
@@ -0,0 +1,31 @@
+from keras.models import load_model
+import numpy as np
+import cv2
+
+model = load_model('keras_model.h5')
+
+data = np.ndarray(shape=(1, 224, 224, 3), dtype=np.float32)
+
+cap = cv2.VideoCapture(0)
+
+while(True):
+            ret, frame = cap.read()
+            cv2.imshow('frame',frame)
+    
+            frame1 = cv2.resize(frame,(224, 224))
+            image_array = np.asarray(frame1)
+
+            normalized_image_array = (image_array.astype(np.float32) / 127.0) - 1
+
+            data[0] = normalized_image_array
+
+            
+            prediction = model.predict(data)
+            print(prediction)
+            
+            
+            if cv2.waitKey(700) & 0xFF == ord('q'):
+                break
+    
+cap.release()
+cv2.destroyAllWindows()
